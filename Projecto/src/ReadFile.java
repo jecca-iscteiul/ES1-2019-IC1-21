29d275f78d806f47235f96278aeec72383e813a4
Eduardo <jecca@iscte-iul.pt>
2019-11-12 21:16:48 +0000

    teste_commit_no_trello

--- Projecto/src/ReadFile.java
+++ Projecto/src/ReadFile.java
@@ -1,6 +1,16 @@
 
 public class ReadFile {
 
+	private String path;
+
+
+
+	public ReadFile(String caminho) {
+		this.path=caminho;
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
