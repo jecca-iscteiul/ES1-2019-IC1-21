5ce62b44e85fd36915ce0eb240b26eab203b3398
Eduardo <jecca@iscte-iul.pt>
2019-11-12 21:47:15 +0000

    fucniona??

--- Projecto/src/ReadFile.java
+++ Projecto/src/ReadFile.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class ReadFile {
 
@@ -7,12 +10,28 @@ public class ReadFile {
 
 	public ReadFile(String caminho) {
 		this.path=caminho;
+		System.out.println(caminho);
 	}
 	
+	void ler() {
+		try {
+			Scanner scanner = new Scanner(new File(path));
+			while(scanner.hasNextLine()) {
+				System.out.println(scanner.nextLine());
+			}
+			
+			scanner.close();
+			
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("erro");
+		}
+	}
 	
-	
+	//fazer getPATH
+	//so da para ler no destktop
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new ReadFile("C:/Users/Eduardo/Desktop/lLong-Method.xlsx").ler();
 
 	}
 
