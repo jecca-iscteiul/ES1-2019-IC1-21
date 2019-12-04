95a917f9ac5489802f87d6bbfab877672f0ac265
Irina Fernandes <iriifernandes@gmail.com>
2019-12-04 14:25:02 +0000

    Isso não dá... Tou-me a passar :(

--- Projecto/src/InterfaceGrafica.java
+++ Projecto/src/InterfaceGrafica.java
@@ -20,9 +20,7 @@ public class InterfaceGrafica {
 	private JFrame frame;
 	private JList<String> list;
 	private DefaultListModel listModel = new DefaultListModel<>();
-
 	private JFrame janelaExibirMetricas;
-
 
 
 
@@ -30,20 +28,36 @@ public class InterfaceGrafica {
 	public InterfaceGrafica() {
 
 		frame = new JFrame("App para detetar a qualidade de defeitos");
-
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
 		frame.setLayout(new BorderLayout());
-
 		frame.setPreferredSize(new Dimension(900, 700));
-
 		addFrameContent();
-
 		frame.pack();
-
 		frame.setResizable(false);
-
 		frame.setVisible(true);
+
+	}
+
+
+	// ISSO É PARA SE ALTERAR --- TEMOS QUE LER DO FICHEIRO
+	private void updateList() {
+
+		listModel.addElement("LOC e CYCLO");
+		listModel.addElement("ATFD e LAA");
+		list = new JList<>(listModel);
+		janelaExibirMetricas.add(list, BorderLayout.CENTER);
+
+	}
+
+
+	private void updateMetricas() {
+
+		janelaExibirMetricas.setLayout(new BorderLayout());
+		janelaExibirMetricas.setPreferredSize(new Dimension(300,300) );
+		janelaExibirMetricas.pack();
+		janelaExibirMetricas.setVisible(true);
+
+		updateList();
 
 	}
 
@@ -51,66 +65,73 @@ public class InterfaceGrafica {
 
 	private void addFrameContent() {
 
-		JButton escolherMetricas = new JButton("Escolher métricas");
-
-		// ISSO É PARA SE ALTERAR --- TEMOS QUE LER DO FICHEIRO
-		listModel.addElement("LOC e CYCLO");
-		listModel.addElement("ATFD e LAA");
-
-		list = new JList<>(listModel);
-
-		escolherMetricas.addActionListener(new ActionListener() {
+		JButton definirThresholds = new JButton("Definir thresholds");
+		definirThresholds.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
-				janelaExibirMetricas = new JFrame("Exibindo métricas");
-				janelaExibirMetricas.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-				janelaExibirMetricas.setPreferredSize(new Dimension(300,300) );
-				janelaExibirMetricas.pack();
-				janelaExibirMetricas.setVisible(true);
-
-				janelaExibirMetricas.add(list);
-
-				String metricaEscolhida = list.getSelectedValue();
-				System.out.println(metricaEscolhida);
+				janelaExibirMetricas = new JFrame("Escolha as métricas");
+				updateMetricas();
 
 
-				if(metricaEscolhida.equals("LOC e CYCLO")) {
-					JTextArea area1 = new JTextArea("Escolha threshold para LOC");
-					area1.setPreferredSize(new Dimension (500,500));
-					area1.setVisible(true);
-					area1.setEditable(true);
-					int limite1 = Integer.parseInt(area1.getText());
+				JButton escolher = new JButton("Escolher");
+				escolher.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						String name = list.getSelectedValue();
+						System.out.println(name);
+
+						if(list.getSelectedIndex() >= 0) {
+
+							JFrame f1 = new JFrame ("Valores");
+							f1.setLayout(new BorderLayout());
+							f1.setPreferredSize(new Dimension(200,200));
+							f1.pack();
+							f1.setResizable(false);
+							f1.setVisible(true);
+
+							JTextArea area1 = new JTextArea();
+							area1.setPreferredSize(new Dimension (200,200));
+							area1.setVisible(true);
+							area1.setEditable(true);
+							String limite1 = area1.getText();
+							System.out.println(limite1);
+
+							f1.add(area1, BorderLayout.NORTH);
+
+						}
 
 
-					janelaExibirMetricas.add(area1);
-				}
+					}
+				});
 
 
-
+				janelaExibirMetricas.add(escolher, BorderLayout.SOUTH);
 
 			}
 		});
 
-		//		JButton denifirThresholds = new JButton("Definir thresholds");
-		//		denifirThresholds.addActionListener(new ActionListener() {
-		//			
-		//			@Override 
-		//			public void actionPerformed(ActionEvent e) {
-		//				
-		//				JTextArea thresholdsUser = new JTextArea("Escreva ");
-		//				
-		//			}
-		//		});
-		//		
+
+
+		JButton mostrarFicheiro = new JButton("Mostrar Ficheiro");
+		mostrarFicheiro.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// COLOCAR AQUI A PARTE DO EDUARDO  --- INTERLIGAR ESSAS DUAS PARTES
+				
+			}
+		});
 
 
 
 		JPanel painel = new JPanel();
 		painel.setBackground(Color.gray);
-		//		painel.add(denifirThresholds);
-		painel.add(escolherMetricas);
+		painel.add(mostrarFicheiro);
+		painel.add(definirThresholds);
+		
 
 
 
