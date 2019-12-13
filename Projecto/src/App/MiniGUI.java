8352b87837293e62eee5856eab1457d40eba94fe
Eduardo <jecca@iscte-iul.pt>
2019-12-13 10:48:20 +0000

    javadoc MiniGUI

--- Projecto/src/App/MiniGUI.java
+++ Projecto/src/App/MiniGUI.java
@@ -25,56 +25,114 @@ import javax.swing.event.ListSelectionListener;
 import Essenciais.*;
 
 /**
+ * Date: 13/12/2019
+ * Esta classe é o intermediário entre o utelizador com o códico, mais especificamente define as regras
  * @author Eduardo
+ * @version 1.0
  *
  */
+
 public class MiniGUI {
 
 
+	/**
+	 * frame é a janela 
+	 */
 	private JFrame frame;
+	/**
+	 * Utelizado para termos aceesso à classe ReadFile
+	 */
 	private ReadFile lerFicheiro;
+	/**
+	 * Tem todos os botoes desta frame
+	 */
 	private JButton[] lista_botoes;
+	/**
+	 * Utelizado para diferenciar as regras criadas no EvenSmalGUI
+	 */
 	private int id;
+	
+	/**
+	 * adicionar elementos a uma JList
+	 */
 	private DefaultListModel modelo = new DefaultListModel<>();
+	/**
+	 * A JList que tem as regras criadas
+	 */
 	private JList<TuploRegra> texto_list;
 
-
+	/**
+	 * A informação auxiliar onde armazena os TuplosRegras
+	 */
 	private ArrayList<TuploRegra> auxTuploRegras = new ArrayList<>();
+	/**
+	 * A informação onde se armazena RegrasCombinadas
+	 */
 	private ArrayList<RegraCombinada> listaRegras = new ArrayList<RegraCombinada>();
 
+	/**
+	 * Verificar se a checkBox "E" foi selecionada
+	 */
 	private boolean eSelecionado;
+	/**
+	 * Verificar se a checkBox "OU" foi selecionada
+	 */
 	private boolean ouSelecionado;
+	/**
+	 * Verificar se a checkBox "isFeatureEnvy" foi selecionada
+	 */
 	private boolean isFeatureEnvy;
+	/**
+	 * Verificar se a checkBox "isLongMethod" foi selecionada
+	 */
 	private boolean isLongMethod;
 
+	/**
+	 * Serve para passar a informação das regras para InterfaceGrafica
+	 */
 	private InterfaceGrafica mainGUI;
 
+	/**
+	 * Inicializar a janela toda, adicionar os botoes, jList, CheckBox ...
+	 * @param lerFicheiro para ter aceeso ao objecto ReadFile
+	 * @param mainGUI para ter aceeso ao objecto InterfaceGrafica
+	 */
 	public MiniGUI(ReadFile lerFicheiro, InterfaceGrafica mainGUI){
 		this.id=0;
 		this.lerFicheiro=lerFicheiro;
 		this.mainGUI = mainGUI;
 		inicializar();
 		addBotao();
-		textovertical();
-		selecionarJlist();
+		CriaJlist();
 		frame.setVisible(true);
 
 
 	}
 
+	/**
+	 * id para diferenciar as regras
+	 * Começa no 1 e vai sempre incrementando
+	 * @return id
+	 */
 	public int getID() {
 		id++;
 		return id;
 	}
 
 
+	/**
+	 * Adiciona informação a Jlist e armazena no ArrayList
+	 * @param tuplo
+	 */
 	public void trataTuplo(TuploRegra tuplo) {
 		modelo.addElement(tuplo);
 		auxTuploRegras.add(tuplo);
-
 	}
 
 
+	/**
+	 * inicializar a frame
+	 */
 	private void inicializar() {
 		frame= new JFrame (lerFicheiro.getnomeFicheiro() + "--> Definir Thresholds");
 		//frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -85,6 +143,9 @@ public class MiniGUI {
 		frame.setLayout(new BorderLayout());
 	}
 
+	/**
+	 * inicializar os botoes, criar os eventos dos botoes e CheckBox
+	 */
 	private void addBotao() {
 		JPanel painel_baixo=new JPanel();
 		painel_baixo.setLayout(new FlowLayout());
@@ -172,13 +233,20 @@ public class MiniGUI {
 		frame.add(painel_baixo, BorderLayout.SOUTH);
 	}
 
-	public void textovertical() {
+	
+	/**
+	 * Criar o o JList e adiciona-lo à frame
+	 */
+	public void CriaJlist() {
 
 		texto_list=new JList<>(modelo);
 		frame.add(texto_list);
 
 	}
 
+	/**
+	 * Cria Regras combinadas e adiciona a list e à interfaceGrafica
+	 */
 	private void criaRegraCombinada() {
 		RegraCombinada reg = null;
 		List<TuploRegra> lista = texto_list.getSelectedValuesList();
@@ -203,6 +271,9 @@ public class MiniGUI {
 	}
 
 
+	/**
+	 * Cria Regras Simples e adiciona a list e à interfaceGrafica
+	 */
 	private void criaRegraSimples() {
 
 		RegraSimples reg = new RegraSimples(texto_list.getSelectedValue());
@@ -215,27 +286,12 @@ public class MiniGUI {
 	}
 
 
-	public void selecionarJlist() {
-		texto_list.addListSelectionListener(new ListSelectionListener(){
-			@Override
-			public void valueChanged(ListSelectionEvent arg0) {
-				if (!arg0.getValueIsAdjusting()) {	
-				//	System.out.println(texto_list.getSelectedValue().toString() + " está selecionado");
-				}
-			}
-		});
-	}
-
-
+	/**
+	 * @return ArrayList de Tuplo de regras
+	 */
 	public ArrayList<TuploRegra> getListTuplos() {
 		return auxTuploRegras;
 	}
-
-	//	public ArrayList<Regra> getListRegras() {
-	//		return listaRegras;
-	//	}
-
-
 
 
 }
